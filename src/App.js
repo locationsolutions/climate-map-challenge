4de1869cdd0f655908680eecd661699b9231aca2
lEGENDARYskyE <43337911+lEGENDARYskyE@users.noreply.github.com>
2019-01-22 20:31:56 +0200

    Edits
    Added interactivity

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, {useEffect, useState} from 'react';
 import Metolib from '@fmidev/metolib';
 import './App.css';
-import {Map, Marker, TileLayer} from "react-leaflet";
+import {Map, Marker, TileLayer, Popup, LayersControl,
+        Circle, LayerGroup, Polyline} from "react-leaflet";
 import styled from "styled-components";
 import L from "leaflet";
 import Sidebar from './Sidebar';
+
+// Santeri Kilpeläinen
+// kilpelainen.santeri@gmail.com
+
+// constructing layers for Overlay control
+const {BaseLayer, Overlay} = LayersControl;
 
 const MapContainer = styled(Map)`
     width: calc(100vw - 300px);
@@ -24,6 +31,7 @@ L.Icon.Default.mergeOptions({
 });
 
 
+
 function App() {
   const [observationLocations, setObservationLocations] = useState([]);
 
@@ -33,7 +41,7 @@ function App() {
     const connection = new Metolib.WfsConnection();
     if (connection.connect('http://opendata.fmi.fi/wfs', 'fmi::observations::weather::cities::multipointcoverage')) {
       connection.getData({
-        begin: Date.now() - 60e3 * 60 * 24 * 6,
+        begin: Date.now() - 60e3 * 60 * 24,
         end: Date.now(),
         requestParameter: "t,snowdepth,r_1h",
         timestep: 60 * 60 * 1000,
@@ -60,19 +68,87 @@ function App() {
     }
   }, []);
 
+  //Polyline Randomizer
+
+  const locArrMax = observationLocations.length - 1;
+  const polylineStartPoint = observationLocations[Math.round(Math.random() * locArrMax)];
+
+
   const position = [65, 26];
   const map = (
     <MapContainer center={position} zoom={6}>
-      <TileLayer
-        url='https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png'
-        attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
-        subdomains='abcd'
-        maxZoom={19}
-      />
-      {observationLocations.map(loc => <Marker position={[loc.position.lat, loc.position.lon]}
-                                               key={loc.info.id} onClick={() => setSelectedLocation(loc.info.id)}>
-      </Marker>)}
-    </MapContainer>
+      <LayersControl position ="topright">
+        <BaseLayer checked name="Basemap">
+          <TileLayer
+            url='https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png'
+            attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
+            subdomains='abcd'
+            maxZoom={19}
+          />
+        </BaseLayer>
+
+        <Overlay checked name="Markers for Locations">
+            <LayerGroup>
+        {observationLocations.map(loc => <Marker position={[loc.position.lon, loc.position.lat]}
+                                                 key={loc.info.id} onClick={() => setSelectedLocation(loc.info.id)}>
+
+        {/* // Popup when clicking marker and giving info from database, data from nearest hour */}
+                                             <Popup>
+                                             <span> <b>{[loc.info.name]}</b><br />
+                                                   WEATHER: {loc && JSON.stringify(loc.data.t.timeValuePairs[24].value)}c<br />
+                                                   SNOW: {loc && JSON.stringify(loc.data.snowdepth.timeValuePairs[24].value)}cm</span>
+                                             </Popup>
+                                        </Marker>
+      )};
+          </LayerGroup>
+      </Overlay>
+
+      <Overlay name="Circles for Snowdepth">
+          <LayerGroup>
+          {observationLocations.map(loc => <Circle center={[loc.position.lon, loc.position.lat]
+                                                  } key={loc.info.id}
+            color="white"
+            radius={loc && Math.abs(JSON.stringify(loc.data.snowdepth.timeValuePairs[0].value))^20000}
+            fillColor="grey"
+            stroke={true}
+            />
+
+          )};
+          </LayerGroup>
+      </Overlay>
+
+      {/* // Balloon by temps, not really working correcty because the scaling */}
+      <Overlay name="Circles for Temps">
+            <LayerGroup>
+              {observationLocations.map(loc => <Circle center={[loc.position.lon, loc.position.lat]
+              } key={loc.info.id}
+              color="grey"
+              radius={loc && (Math.abs(loc.data.t.timeValuePairs[0].value || 2))^20000}
+              fillColor="white"
+              fillOpacity={0.65}
+              fillRule="nonzero"
+              stroke={true}
+              />
+              )};
+
+            </LayerGroup>
+        </Overlay>
+
+        {/* Polylines go to randomly decided place, target changes when clicking on some marker */}
+        <Overlay name ="Polylines to Random() target">
+          <LayerGroup>
+              {
+                observationLocations.map(point => <Polyline positions={[
+                                          [polylineStartPoint.position.lon, polylineStartPoint.position.lat],
+                                          [point.position.lon, point.position.lat]]
+                                        } key={point.info.id} color="lime"
+                    />
+                )};
+          </LayerGroup>
+        </Overlay>
+
+  </LayersControl>
+</MapContainer>
   );
 
   return (
@@ -80,8 +156,6 @@ function App() {
       <Sidebar selectedLocationId={selectedLocation} observationLocations={observationLocations}/>
       {map}
     </div>
-  );
-
+    );
 }
-
 export default App;
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,12 +2,38 @@ import React from 'react';
 import styled from "styled-components";
 import getSelectedLocatoinId from './locationGetter';
 
+
 function Sidebar({selectedLocationId, observationLocations}) {
     const id = getSelectedLocatoinId(selectedLocationId);
 
     const loc = observationLocations.find(loc => loc.info.id === id);
     return <div>
-        <pre>{loc && JSON.stringify(loc.info, null, 4)}</pre>
+    
+            { /* // Clicked place and region */}
+        <pre><h3>Place: {loc && String(loc.info.name, null, 4)}</h3>
+             <h2>Region: {loc && String(loc.info.region, null, 4)}</h2>
+
+            { /* // Closest to current time */ }
+            <h3>Weather at {loc && String(new Date(loc.data.t.timeValuePairs[24].time).toLocaleTimeString())} </h3>
+            <li>Temp: {loc && String(loc.data.t.timeValuePairs[24].value || "no data ")  + String(loc.data.t.property.unit)} </li>
+            <li>Snowdepth: {loc && String(loc.data.snowdepth.timeValuePairs[24].value || "no data ") + String(loc.data.snowdepth.property.unit)} </li>
+
+            { /*   // 3 hours from current time */ }
+            <h3>Weather was at {loc && String(new Date(loc.data.t.timeValuePairs[21].time).toLocaleTimeString())} </h3>
+            <li>Temp: {loc && String(loc.data.t.timeValuePairs[21].value || "no data ") + String(loc.data.t.property.unit)} </li>
+            <li>Snowdepth: {loc && String(loc.data.snowdepth.timeValuePairs[21].value || "no data ") + String(loc.data.snowdepth.property.unit)} </li>
+
+            { /*   // 6 hours from current time */ }
+            <h3>Weather was at {loc && String(new Date(loc.data.t.timeValuePairs[18].time).toLocaleTimeString())} </h3>
+            <li>Temp: {loc && String(loc.data.t.timeValuePairs[18].value || "no data ") + String(loc.data.t.property.unit)}  </li>
+            <li>Snowdepth: {loc && String(loc.data.snowdepth.timeValuePairs[18].value || "no data ") + String(loc.data.snowdepth.property.unit)} </li>
+
+            { /*   // 9 hours from current time */ }
+            <h3>Weather was at {loc && String(new Date(loc.data.t.timeValuePairs[15].time).toLocaleTimeString())} </h3>
+            <li>Temp: {loc && String(loc.data.t.timeValuePairs[15].value || "no data ") + String(loc.data.t.property.unit)}  </li>
+            <li>Snowdepth: {loc && String(loc.data.snowdepth.timeValuePairs[15].value || "no data ") + String(loc.data.snowdepth.property.unit)} </li>
+
+        </pre>
     </div>
 }
 
