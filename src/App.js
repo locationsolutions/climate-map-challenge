778ee7f9e9e117bdf39d0d38e821233f2cb94db8
timoijas <timo.ok.ijas@gmail.com>
2020-11-04 21:38:33 +0200

    added graphs

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import L from "leaflet";
 import Sidebar from './Sidebar';
 import getSelectedLocatoinId from './locationGetter';
 import Chart from "chart.js";
+import {Line} from 'react-chartjs-2';
+import {popupContent, popupHead, okText, loadingPopup} from "./PopupStyles";
+import idwLayer from "C:/Users/tipe_/Documents/Projekteja/Koodaus/CGI_climate/climate-map-challenge/node_modules/leaflet.idw/src/leaflet-idw.js"
 
 
 const MapContainer = styled(Map)`
@@ -29,19 +32,96 @@ L.Icon.Default.mergeOptions({
 
 function App() {
   const [observationLocations, setObservationLocations] = useState([]);
-
   const [selectedLocation, setSelectedLocation] = useState(null);
 
   const clickedLocation = observationLocations.find(loc=> loc.info.id === selectedLocation);
-
   var clickedData = null;
-  if (selectedLocation != null) {
-    clickedData = clickedLocation.data.t.timeValuePairs
-    console.log(JSON.stringify(clickedData));
+  var timeList2 = null;
+  var valListT = null;
+  var valListSnow = null;
+
+  var data = null;
+  var options = null;
+  const [loading, setLoading] = useState(true);
+
+  if (selectedLocation) {
+    clickedData = clickedLocation.data
+    //console.log(JSON.stringify(clickedData));
+
+    timeList2=  clickedData.t.timeValuePairs.map(row=> ''+ new Date(row.time));
+    valListT = clickedData.t.timeValuePairs.map(row=> row.value);
+    valListSnow = clickedData.snowdepth.timeValuePairs.map(row=> row.value);
+
+    console.log(JSON.stringify(valListT.length));
     // create line chart
+    //labels: timeList,
+    data = {
+      labels: timeList2,
+      datasets: [
+        {
+          label: 'Temperature: ',
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(72,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderCapStyle: 'butt',
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: valListT
+        },
+        {
+          label: 'Snowdepth: ',
+          fill: true,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(72,192,192,0.4)',
+          borderColor: 'rgba(75,0,0,1)',
+          borderCapStyle: 'butt',
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,0,0,1)',
+          pointBackgroundColor: '#fff',
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,0,0,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: valListSnow
+        }
 
+      ]
+    };
+
+    options = {
+      scales: {
+        xAxes: [
+          {
+            type: "time",
+            labels: timeList2,
+            time: {
+              unit: 'day'
+            }
+          }
+        ],
+        yAxes: [
+          {
+            ticks: {
+              min: -12,
+              max: 15
+            }
+          }
+        ]
+      }
+    }
   }
-
 
 
   useEffect(function fetchObservationLocations() {
@@ -70,6 +150,7 @@ function App() {
           );
 
           connection.disconnect();
+          setLoading(false);
         }
       });
     }
@@ -77,6 +158,8 @@ function App() {
 
   const position = [65, 26];
   const map = (
+
+
     <MapContainer center={position} zoom={6}>
       <TileLayer
         url='https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png'
@@ -85,34 +168,60 @@ function App() {
         maxZoom={19}
       />
 
+
       {observationLocations.map(loc => (
         <Marker position={[loc.position.lon, loc.position.lat]}
                 onClick={() => setSelectedLocation(loc.info.id)}
                 key={loc.info.id}>
         </Marker>
-      ))}
+      ))
+
+      }
 
 
       {selectedLocation && (
-        <Popup
+        <Popup classname = "graphPopup"
           position = {[clickedLocation.position.lon, clickedLocation.position.lat]}
+          maxWidth= "800px"
           onClose= {()=> {
             setSelectedLocation(null);
           }}
-        >
-          <div>
-            <h2>{ clickedLocation.info.name }</h2>
-            <p>{
-                // insert line chart
-                "Position: " + JSON.stringify(clickedLocation.info.position, null, 4)
 
-              }
-            </p>
+        >
+          <div style = {popupHead}>
+            <h2>{"Station: "+ clickedLocation.info.name }</h2>
+          </div>
+
+          <div style = {popupContent}>
+            <Line
+              data={data}
+              height={400}
+              width={500}
+              options = {options}
+            />
           </div>
         </Popup>
       )}
+
+      {loading &&(
+        <Popup
+          classname = "loadingPopup"
+          position = {position}
+          maxWidth= "800px"
+
+        >
+          <div style = {loadingPopup}>
+            <h2>{"Loading Data..."}</h2>
+          </div>
+        </Popup>
+
+
+
+      )}
     </MapContainer>
   );
+
+
 
   return (
     <div className="App">
---
+++ src/PopupStyles.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from "styled-components";
+
+const popupContent = {
+  height: "400px",
+  width: "500px",
+  marginTop: "20px"
+};
+
+const popupHead = {
+  fontWeight: "bold",
+  fontSize: "16px"
+
+};
+
+const popupText = {
+  fontSize: "15px",
+  marginBottom: "20px"
+};
+
+const okText = {
+  fontSize: "15px",
+  textAlign: "center"
+
+};
+
+const loadingPopup = {
+  fontWeight: "bold",
+  fontSize: "16px",
+  color: "red"
+
+}
+
+export { popupContent, popupHead, popupText, okText, loadingPopup };
