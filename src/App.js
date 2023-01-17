863c6d7b7cd27ab3ef1d56dd50f03f651e0d6510
Jani Martikainen <jani.martikainen@into-digital.fi>
2023-01-17 17:10:14 +0200

    Removed Sidebar, added popups

--- src/App.js
+++ src/App.js
@@ -1,37 +1,32 @@
 import React, {useEffect, useState} from 'react';
 import Metolib from '@fmidev/metolib';
 import './App.css';
-import {Map, Marker, TileLayer} from "react-leaflet";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import styled from "styled-components";
 import L from "leaflet";
-import Sidebar from './Sidebar';
 
 const MapContainer = styled(Map)`
-    width: calc(100vw - 300px);
-    height: 100vh;
-    position:absolute;
-    top:0px;
-    left:300px;
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  top: 0px;
+  left: 0px;
 `;
-
 
 // Ugly hack to fix Leaflet icons with leaflet loaders
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
 });
-
 
 function App() {
   const [observationLocations, setObservationLocations] = useState([]);
 
-  const [selectedLocation, setSelectedLocation] = useState(null);
-
   useEffect(function fetchObservationLocations() {
     const connection = new Metolib.WfsConnection();
-    if (connection.connect('http://opendata.fmi.fi/wfs', 'fmi::observations::weather::cities::multipointcoverage')) {
+    if (connection.connect( "http://opendata.fmi.fi/wfs", "fmi::observations::weather::cities::multipointcoverage" )) {
       connection.getData({
         begin: Date.now() - 60e3 * 60 * 24 * 6,
         end: Date.now(),
@@ -40,48 +35,42 @@ function App() {
         bbox: "20.6455928891, 59.846373196, 31.5160921567, 70.1641930203",
         callback: (data, errors) => {
           if (errors.length > 0) {
-
-            errors.forEach(err => {
-              console.error('FMI API error: ' + err.errorText);
+            errors.forEach((err) => {
+              console.error("FMI API error: " + err.errorText);
             });
             return;
           }
 
-          setObservationLocations(data.locations
-            .map(loc => {
+          setObservationLocations(data.locations.map((loc) => {
+              console.log(loc);
               const [lat, lon] = loc.info.position.map(parseFloat);
-              return {...loc, position: {lat, lon}}
-            })
-          );
+              return { ...loc, position: { lat, lon } };
+            }));
 
           connection.disconnect();
-        }
+        },
       });
     }
   }, []);
 
   const position = [65, 26];
-  const map = (
-    <MapContainer center={position} zoom={6}>
-      <TileLayer
-        url='https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png'
-        attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
-        subdomains='abcd'
-        maxZoom={19}
-      />
-      {observationLocations.map(loc => <Marker position={[loc.position.lat, loc.position.lon]}
-                                               key={loc.info.id} onClick={() => setSelectedLocation(loc.info.id)}>
-      </Marker>)}
-    </MapContainer>
-  );
+  const map = <MapContainer center={position} zoom={6}>
+      <TileLayer url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png" attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>' subdomains="abcd" maxZoom={19} />
+      {observationLocations.map((loc) => (
+        <Marker
+          position={[loc.position.lat, loc.position.lon]}
+          key={loc.info.id}
+          //   onClick={() => setSelectedLocation(loc.info.id)}
+        >
+          <Popup>
+            <p>Tarkkailupiste: {loc.info.name}</p>
+            <p>Alue: {loc.info.region}</p>
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>;
 
-  return (
-    <div className="App">
-      <Sidebar selectedLocationId={selectedLocation} observationLocations={observationLocations}/>
-      {map}
-    </div>
-  );
-
+  return <div className="App">{map}</div>;
 }
 
 export default App;
