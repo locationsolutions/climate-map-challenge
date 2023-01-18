388fd2491460cdf9b627c1d68fe0ed3150e6c730
crowelian <harriahola@live.fi>
2023-01-18 17:11:21 +0200

    rearrange files a bit and reformatted App.js

---
+++ src/components/Sidebar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+import getSelectedLocatoinId from "./locationGetter";
+
+function Sidebar({ selectedLocationId, observationLocations }) {
+  const id = getSelectedLocatoinId(selectedLocationId);
+
+  const loc = observationLocations.find((loc) => loc.info.id === id);
+  return (
+    <div>
+      <pre>{loc && JSON.stringify(loc.info, null, 4)}</pre>
+    </div>
+  );
+}
+
+export default styled(Sidebar)`
+  width: 300px;
+  height: 100vh;
+`;
